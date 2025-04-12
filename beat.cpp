7d7e6c3669275fdc303cb7f3fbfd524e117e4d09
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-04-12 23:29:56 +0200

    criado o visualizador de musica  com cpp,ele analisa um audio  "arquivo.wav" mostrado barras animadas no terminal  que sobem e descem e usa  al lib ncurse parao visual

---
+++ beat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <sndfile.h>
+#include <fftw3.h>
+#include <ncurses.h>
+
+using namespace std;
+
+
+int main(){
+
+    return 0;
+}
