4d7fd7337e85954b70f73be368bc02fa0a9a95ec
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-04-12 23:42:23 +0200

    desenvolve a funcao drawBars

--- beat.cpp
+++ beat.cpp
@@ -5,8 +5,17 @@
 #include <fftw3.h>
 #include <ncurses.h>
 
+#define FRAME_SIZE 1024
+
 using namespace std;
 
+void drawBars(const vector<double> & magnitude){
+    clear();
+    int cols = COLS;
+    int height = LINES;
+
+    
+}
 
 int main(){
 
